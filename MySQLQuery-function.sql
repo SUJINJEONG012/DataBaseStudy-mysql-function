d16a8893cf4f91616dd51e4a7d8d987f7d79feb5
angela <peekaboo32@naver.com>
2023-07-11 14:22:37 +0900

    elt() , field()위치반환 함수 사용

--- MySQLQuery-function.sql
+++ MySQLQuery-function.sql
@@ -12,3 +12,19 @@ select memid, memid, ordamt from order_h where left(orderno, 6) = '202201';
 
 -- right() , 으론쪽에서 부터 부분문자열을 반환한다.
 select right(orderno,6) from order_h;
+
+-- *** substring(), substr(), mid() *** 함수 => 함수 이름만 다를 뿐 같은 기능의 함수
+
+-- 인수로 주어진 문자열 상수 또는 변수/열의 값에서 지정한 위치부터 문자 수 만큼의 부분 문자열을 반환하고, 
+-- 문자 수를 생략하면 지정한 위치부터 문자열 끝까지의 부분문자열을 반환한다.
+
+select substring(orderno, 5,2) from order_h; -- 다섯번째순서, 문자수
+
+-- elt() field() find_in_set() 함수
+select elt(2,'월','화','수','목','금', '토'); -- 화 , 숫자번째의 문자열을 반환
+select elt(7,'월','화','수','목','금', '토'); -- null , 숫자번째인 항목이 없을땐 null반환
+
+select field('수', '월', '화', '수', '목','금','토'); -- 3 , 위치를 반환
+select field('일', '월', '화', '수', '목','금','토'); -- 0 , 숫자번째인 항목이 없을땐 null반환
+
+
